49c50e0a5dbf24e878c6cf6dfb3b5f5b3af5c688
Hatice KATRANCI AKGÜL <76768140+haticekatranciakgul@users.noreply.github.com>
2024-04-16 17:17:19 +0300

    form

--- formik-react/src/components/SignUp/Signup.js
+++ formik-react/src/components/SignUp/Signup.js
@@ -4,54 +4,75 @@ import validationSchema from '../validation/Validations';
 
 function Signup() {
 
-    const {handleSubmit, handleChange, values } = useFormik({
-        initialValues: {
-          email: '',
-          password:'',
-          passwordConfirm:''
-            
-        },
-        onSubmit: values => {
-          console.log(values);
-    
-        },
-        validationSchema
-      });
+  const { handleSubmit, handleChange, handleBlur, values, errors, touched } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      passwordConfirm: ''
+
+    },
+    onSubmit: values => {
+      console.log(values);
+
+    },
+    validationSchema
+  });
+
 
   return (
     <div>
-         <h1>Sign Up</h1>
-            <form onSubmit={handleSubmit}>
-              <label >Email</label>
-              <input name='email' value={values.email} onChange={handleChange}></input>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label >Email</label>
+        <input
+          name='email'
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        ></input>
 
-              <br></br>
-              <br></br>
-              <label >Password</label>
-              <input name='password' value={values.password} onChange={handleChange}></input>
-
-              <br></br>
-              <br></br>
-
-              <label >Password Confirm</label>
-              <input name='passwordConfirm' value={values.passwordConfirm} onChange={handleChange}></input>
-
-              <br></br>
-              <br></br>
+        {errors.email && touched.email && <div className='error'>{errors.email}</div>}
 
 
+        <br></br>
+        <br></br>
 
-              <button type="submit">Submit</button>
+        <label >Password</label>
+        <input
+          name='password'
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        ></input>
+
+        {errors.password && touched.password && <div className='error'>{errors.password}</div>}
+
+        <br></br>
+        <br></br>
+
+        <label >Password Confirm</label>
+        <input
+          name='passwordConfirm'
+          value={values.passwordConfirm}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        ></input>
+
+        {errors.passwordConfirm && touched.passwordConfirm && <div className='error'>{errors.passwordConfirm}</div>}
+
+        <br></br>
+        <br></br>
+
+        <button type="submit">Submit</button>
+
+        <br></br>
+        <br></br>
 
 
-              <br></br>
-              <br></br>
+        {JSON.stringify(values)}
 
+      </form>
 
-              {JSON.stringify(values)}
-
-            </form>
-      
     </div>
   )
 }
